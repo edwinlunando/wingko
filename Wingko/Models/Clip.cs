606c80592cd1e33c86ba15f18e039baf50ad2dc1
Edwin Lunando <edwinlunando@gmail.com>
2016-09-14 20:44:36 +0700

    first commit

---
+++ Wingko/Models/Clip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Wingko.Models
+{
+    class Clip
+    {
+
+        enum ClipType { Text, FileDrop, Image, Data, Audio};
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public int Type { get; set; }
+    }
+}
---
+++ Wingko/Services/ClipService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Wingko.Services
+{
+    class ClipService
+    {
+
+        private SQLiteConnection _database;
+
+        public ClipService()
+        {
+            _database = new SQLiteConnection("wingko.db");
+        }
+
+
+
+    }
+}
